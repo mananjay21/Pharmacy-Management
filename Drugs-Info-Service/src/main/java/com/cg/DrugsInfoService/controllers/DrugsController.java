8462f2aa81bc3a78f066cc3871a2183bedce2946
Mananjay Roy <roymananjay@gmail.com>
2022-09-15 01:39:27 +0530

    Landing Page Update

--- Drugs-Info-Service/src/main/java/com/cg/DrugsInfoService/controllers/DrugsController.java
+++ Drugs-Info-Service/src/main/java/com/cg/DrugsInfoService/controllers/DrugsController.java
@@ -13,6 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/drugs")
+@CrossOrigin
 public class DrugsController {
     @Autowired
     private SequenceGeneratorService sequenceGeneratorService;
